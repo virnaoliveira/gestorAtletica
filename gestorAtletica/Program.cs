3d4f277f82aa2b7d7d9432f622d6103c36daa5d9
Virna Oliveira <92312347+virnaoliveira@users.noreply.github.com>
2023-08-23 23:17:51 -0300

    ajuste da string de conexão

--- gestorAtletica/Program.cs
+++ gestorAtletica/Program.cs
@@ -7,7 +7,7 @@ namespace MembrosApp
     {
         static void Main(string[] args)
         {
-            string connectionString = "Host=database-1.cqgole4d5zz1.us-east-1.rds.amazonaws.com;Port=5432;Database=postgres;Username=postgres;Password=admin123;SSL Mode=Prefer;Timeout=10;";
+            string connectionString = "Host=database-1.cqgole4d5zz1.us-east-1.rds.amazonaws.com;Port=5432;Database=postgres;Username=postgres;Password=admin123;Timeout=10;";
 
             using (var connection = new NpgsqlConnection(connectionString))
             {
@@ -18,43 +18,23 @@ namespace MembrosApp
                 Console.WriteLine("1. Incluir Membro");
                 Console.WriteLine("2. Visualizar Lista de Membros");
 
-                int choice = int.Parse(Console.ReadLine());
+                int opcao = int.Parse(Console.ReadLine());
 
-                switch (choice)
+                switch (opcao)
                 {
                     case 1:
-                        Console.Write("Digite a matrícula do membro: ");
-                        string matricula = Console.ReadLine();
-                        Console.Write("Digite o nome do membro: ");
-                        string nome = Console.ReadLine();
-                        Console.Write("Digite o sobrenome do membro: ");
-                        string sobrenome = Console.ReadLine();
-                        Console.Write("Digite a data de nascimento do membro (yyyy-MM-dd): ");
-                        string dataNascimento = Console.ReadLine();
-                        Console.Write("Digite o email do membro: ");
-                        string email = Console.ReadLine();
-                        Console.Write("Digite o telefone do membro: ");
-                        string telefone = Console.ReadLine();
-                        Console.Write("Digite o curso do membro: ");
-                        string curso = Console.ReadLine();
-                        Console.Write("Digite o status do membro: ");
-                        string status = Console.ReadLine();
-                        Console.Write("Digite o CPF do membro: ");
-                        string cpf = Console.ReadLine();
-                        Console.Write("Digite a senha do membro: ");
-                        string senha = Console.ReadLine();
-                        Console.Write("Digite a rua do membro: ");
-                        string rua = Console.ReadLine();
-                        Console.Write("Digite o bairro do membro: ");
-                        string bairro = Console.ReadLine();
-                        Console.Write("Digite o número do membro: ");
-                        string numero = Console.ReadLine();
-                        Console.Write("Digite o complemento do membro: ");
-                        string complemento = Console.ReadLine();
-                        IncluirMembro(connection, matricula, nome, sobrenome, dataNascimento, email, telefone, curso, status, cpf, senha, rua, bairro, numero, complemento);
+                        Console.Write("Digite o nome do plano: ");
+                        string plano = Console.ReadLine();
+                        Console.Write("Digite as vantagens do plano: ");
+                        string vantagens = Console.ReadLine();
+                        Console.Write("Digite a duração do plano em meses: ");
+                        int duracao = int.Parse(Console.ReadLine());
+                        Console.Write("Digite o preço do plano: (Ex: 49,99)");
+                        decimal preco = decimal.Parse(Console.ReadLine());
+                        IncluirPlano(connection, plano, vantagens, duracao, preco);
                         break;
                     case 2:
-                        VisualizarMembros(connection);
+                        VisualizarPlanos(connection);
                         break;
                     default:
                         Console.WriteLine("Escolha inválida!");
@@ -63,38 +43,35 @@ namespace MembrosApp
             }
         }
 
-        static void IncluirMembro(NpgsqlConnection connection, string matricula, string nome, string sobrenome, string dataNascimento, string email, string telefone, string curso, string status, string cpf, string senha, string rua, string bairro, string numero, string complemento)
+        static void IncluirPlano(NpgsqlConnection connection, string plano, string vantagens, int duracao, decimal preco)
         {
-            using (var command = new NpgsqlCommand("INSERT INTO atleticagestor.membro(matricula, nome, sobrenome, data_nascimento, email, telefone, curso, status, cpf, senha, rua, bairro, numero, complemento) VALUES (@matricula, @nome, @sobrenome, @dataNascimento, @email, @telefone, @curso, @status, @cpf, @senha, @rua, @bairro, @numero, @complemento)", connection))
+            using (var command = new NpgsqlCommand("INSERT INTO atleticagestor.plano(nome, vantagens, duracao, preco) VALUES (@plano, @vantagens, @duracao, @preco)", connection))
             {
-                command.Parameters.AddWithValue("matricula", matricula);
-                command.Parameters.AddWithValue("nome", nome);
-                command.Parameters.AddWithValue("sobrenome", sobrenome);
-                command.Parameters.AddWithValue("dataNascimento", DateTime.Parse(dataNascimento));
-                command.Parameters.AddWithValue("email", email);
-                command.Parameters.AddWithValue("telefone", telefone);
-                command.Parameters.AddWithValue("curso", curso);
-                command.Parameters.AddWithValue("status", status);
-                command.Parameters.AddWithValue("cpf", cpf);
-                command.Parameters.AddWithValue("senha", senha);
-                command.Parameters.AddWithValue("rua", rua);
-                command.Parameters.AddWithValue("bairro", bairro);
-                command.Parameters.AddWithValue("numero", numero);
-                command.Parameters.AddWithValue("complemento", complemento);
+                command.Parameters.AddWithValue("plano", plano);
+                command.Parameters.AddWithValue("vantagens", vantagens);
+                command.Parameters.AddWithValue("duracao", duracao);
+                command.Parameters.AddWithValue("preco", preco);
                 command.ExecuteNonQuery();
-                Console.WriteLine("Membro incluído com sucesso!");
+                Console.WriteLine("Plano incluído com sucesso!");
             }
         }
 
-        static void VisualizarMembros(NpgsqlConnection connection)
+        static void VisualizarPlanos(NpgsqlConnection connection)
         {
-            using (var command = new NpgsqlCommand("SELECT * FROM atleticagestor.membro", connection))
+            using (var command = new NpgsqlCommand("SELECT * FROM atleticagestor.plano", connection))
             using (var reader = command.ExecuteReader())
             {
-                Console.WriteLine("Lista de Membros:");
-                while (reader.Read())
+                if (!reader.HasRows)
                 {
-                    Console.WriteLine($"Matrícula: {reader["matricula"]}, Nome: {reader["nome"]}, Sobrenome: {reader["sobrenome"]}");
+                    Console.WriteLine("Ainda não há planos cadastrados.");
+                    return;
+                } else
+                {
+                    Console.WriteLine("Lista de Planos:");
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"Plano: {reader["nome"]}, Vantagens: {reader["vantagens"]}, Duração: {reader["duracao"]}, Preço: {reader["preco"]}");    
+                    }
                 }
             }
         }
